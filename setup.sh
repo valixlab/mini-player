69c7d33edec15f50f8f8612de4503eaf99fdbf4d
valixlab <valix6643@gmail.com>
2026-06-30 14:08:40 +0200

    Add files via upload
    Ajout des fichiers du projet

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.1
+sudo apt install playerctl
+python3 -m venv venv --system-site-packages
+source venv/bin/activate
+pip3 install pywebview
